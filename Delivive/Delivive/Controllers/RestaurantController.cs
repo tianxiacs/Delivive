ca3cebfed0013eb5fcfd270394a3c0efeb5ca3fd
KaiOoOKai <kaihuang0804@gmail.com>
2020-12-03 15:58:12 -0700

    Add menu is done

--- Delivive/Delivive/Controllers/RestaurantController.cs
+++ Delivive/Delivive/Controllers/RestaurantController.cs
@@ -116,9 +116,85 @@ namespace Delivive.Controllers
 
         public ActionResult AddMenu()
         {
+            string constr = ConfigurationManager.ConnectionStrings["DeliviveConnection"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(constr))
+            {
+                List<FoodModel> result = new List<FoodModel>();
+                string sql = "SELECT * FROM [Food]"
+                            + " WHERE Restaurant_id = " + 1 + ";";
+                using (SqlCommand cmd = new SqlCommand(sql))
+                {
+                    cmd.Connection = con;
+                    con.Open();
+                    using (SqlDataReader sdr = cmd.ExecuteReader())
+                    {
+                        while (sdr.Read())
+                        {
+                            result.Add(new FoodModel
+                            {
+                                Restaurant_id = Convert.ToInt32(sdr["Restaurant_id"]),
+                                Food_id = Convert.ToInt32(sdr["Food_id"]),
+                                Food_name = sdr["Food_name"].ToString(),
+                                Price = Convert.ToInt32(sdr["Price"]),
+                                Description = sdr["Description"].ToString(),
+                            });
+                        }
+                    }
+                    con.Close();
+                }
+                ViewBag.Result = result;
+            }
+
             FoodModel viewModel = new FoodModel();
-            viewModel.Restaurant_id = int.Parse( Session["Restaurant_id"].ToString() );
+            //viewModel.Restaurant_id = int.Parse( Session["Restaurant_id"].ToString() );
+            viewModel.Restaurant_id = 1;
             return View(viewModel);
+        }
+
+        public ActionResult addMenuAction(FoodModel model) 
+        {
+            string constr = ConfigurationManager.ConnectionStrings["DeliviveConnection"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(constr))
+            {
+                List<FoodModel> result = new List<FoodModel>();
+                string sql = "INSERT INTO Food ([Restaurant_id],[Food_name],[Price],[Description]) VALUES (" +
+                            model.Restaurant_id + ",'" +
+                            model.Food_name + "'," +
+                            model.Price + ",'" +
+                            model.Description + "');";
+                using (SqlCommand cmd = new SqlCommand(sql))
+                {
+                    cmd.Connection = con;
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+                    con.Close();
+                }
+
+                return Json("Submit Succesfully!", JsonRequestBehavior.AllowGet);
+            }
+        }
+
+        // NOT DONE YET
+        public ActionResult PlaceOrder(List<FoodModel> foods)
+        {
+            string constr = ConfigurationManager.ConnectionStrings["DeliviveConnection"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(constr))
+            {
+                List<FoodModel> result = new List<FoodModel>();
+                string sql = "INSERT INTO ORDER () VALUES ("
+                            + ");";
+                using (SqlCommand cmd = new SqlCommand(sql))
+                {
+                    cmd.Connection = con;
+                    con.Open();
+                    
+                    con.Close();
+                }
+
+                return Json("Successfully");
+            }
+
+            return Json("Successfully");
         }
     }
 }
