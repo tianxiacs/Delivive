ef0c6bdd15cc9f6eb5d9a628f1392f66b9b3f2d2
KaiOoOKai <kaihuang0804@gmail.com>
2020-12-04 13:39:08 -0700

    restaurantapplication

--- Delivive/Delivive/Controllers/RestaurantController.cs
+++ Delivive/Delivive/Controllers/RestaurantController.cs
@@ -14,7 +14,31 @@ namespace Delivive.Controllers
         // GET: Restaurant
         public ActionResult RestaurantApplication()
         {
-            return View();
+            RestaurantApplicationModel viewModel = new RestaurantApplicationModel();
+            return View(viewModel);
+        }
+
+        public ActionResult submitRestaurantApplication(RestaurantApplicationModel viewModel)
+        {
+            string constr = ConfigurationManager.ConnectionStrings["DeliviveConnection"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(constr))
+            {
+                string sql = "INSERT INTO End_User (Name,Phone,Password) VALUES ('" + viewModel.Name + "','" + viewModel.Phone + "','" + viewModel.Password + "');";
+
+                sql += "INSERT INTO Restaurant (Business_hour, Address, User_id) VALUES ('" + viewModel.Business_hour + "' , '" + viewModel.Address + "',(SELECT SCOPE_IDENTITY()));";
+
+                sql += "INSERT INTO Restaurant_Application (Restaurant_id, [Small_local], [Business_license], Short_answer, Restaurant_Decision)"
+                            + " VALUES ((SELECT SCOPE_IDENTITY()),'" + viewModel.Small_local + "','" + viewModel.Business_license + "','" + viewModel.Short_answer + "','false');";
+                using (SqlCommand cmd = new SqlCommand(sql))
+                {
+                    cmd.Connection = con;
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+                    con.Close();
+                }
+
+                return Json("Submit Succesfully!", JsonRequestBehavior.AllowGet);
+            }
         }
 
         public ActionResult GetRestaurants()
---
+++ Delivive/Delivive/Models/RestaurantApplicationModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Delivive.Models
+{
+    public class RestaurantApplicationModel : RestaurantModel
+    {
+        public bool Restaurant_id { get; set; }
+        [Display(Name = "Small local?")]
+        public bool Small_local { get; set; }
+        [Display(Name = "Business license?")]
+        public bool Business_license { get; set; }
+        public string Short_answer { get; set; }
+        public int? Admin_id { get; set; }
+        public bool Driver_Decision { get; set; }
+    }
+}
